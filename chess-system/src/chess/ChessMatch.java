d297f7c40e60bbac5a8560209cbc410e6d8970cb
Ariane Wwelke <arianewelkep97@gmail.com>
2024-12-12 23:16:06 -0300

    Handling captured pieces

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -7,12 +7,17 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 import java.security.DrbgParameters;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch() {
         //inicia a partida, cria um tabuleiro (8,8) e chamo o InitialSetup
@@ -60,6 +65,12 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null) {
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
+
         return capturedPiece;
     }
 
@@ -91,7 +102,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece,new ChessPosition(column,row).toPosition());
-
+        piecesOnTheBoard.add(piece);
     }
 
     //metodo responsavel por iniciar a partida de Xadrez, colocando as pecas no tabuleiro
