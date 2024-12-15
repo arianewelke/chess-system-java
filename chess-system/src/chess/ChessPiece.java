651837f6bda888ec2adc9419851d349d7a134f23
Ariane Wwelke <arianewelkep97@gmail.com>
2024-12-15 11:01:11 -0300

    Piece move count

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
     //criou o construtor ChessPiece recebendo o Board e o Color. O Board repassa a chamada para o construtor da super classe
     //que e o construtor da classe Piece
@@ -15,9 +16,21 @@ public abstract class ChessPiece extends Piece {
         this.color = color;
     }
 
+    public int getMoveCount() {
+        return moveCount;
+    }
+
     public Color getColor() {
         return color;
     }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
 
     public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
