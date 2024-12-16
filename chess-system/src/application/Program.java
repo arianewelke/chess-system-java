b3b8cef975533fb7c1ddb06c06ad7a41438e7b8c
Ariane Wwelke <arianewelkep97@gmail.com>
2024-12-15 21:46:44 -0300

    Special move - En Passant

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -38,7 +38,8 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-            } catch (ChessException | InputMismatchException e) {
+            }
+            catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
